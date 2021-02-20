34107756e3fcab951c93614493257cee7a0634d6
Ashwin Km <kmuraliashwin@gmail.com>
2021-02-20 23:07:19 +0530

    included the customer middlewares to handle errors

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const products = require("./data/products");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const productRoutes = require("./routes/productRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 
@@ -11,11 +12,20 @@ connectDB();
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   console.log(req.originalUrl);
+//   next();
+// });
+
 app.get("/", (req, res) => {
   res.send("API is running..");
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
