07a556a2948ff74e490c36a7e6ee495e78fe81ca
Ashwin Km <kmuraliashwin@gmail.com>
2021-03-01 23:52:47 +0530

    routed profile details pointing to get profile function in user controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { authUser } = require("../controller/userController.js");
+const { authUser, getUserProfile } = require("../controller/userController.js");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 module.exports = router;
