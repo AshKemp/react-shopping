001a79060b1303d202dcb45f48c13d24e59da2a9
Ashwin Km <kmuraliashwin@gmail.com>
2021-03-02 19:40:20 +0530

    created a method in the controller for registering user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -21,6 +21,33 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   if (user) {
@@ -37,4 +64,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   res.send("Success");
 });
 
-module.exports = { authUser, getUserProfile };
+module.exports = { authUser, registerUser, getUserProfile };
