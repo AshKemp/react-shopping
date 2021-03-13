23f4428ba7d0e1bbe64aaf9eab768b732d796da7
Ashwin Km <kmuraliashwin@gmail.com>
2021-03-13 21:55:59 +0530

    Mounted the route to get an order by id in orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { addOrderItems } = require("../controller/orderController.js");
+const {
+  addOrderItems,
+  getOrderById,
+} = require("../controller/orderController.js");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").post(protect, addOrderItems);
+router.route("/:id").get(protect, getOrderById);
 
 module.exports = router;
