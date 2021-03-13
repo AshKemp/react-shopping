6732b3589e7f5c05353b62f0196b7f4723d0d0ef
Ashwin Km <kmuraliashwin@gmail.com>
2021-03-13 21:00:49 +0530

    Changed currency symbol to dollar in Cart Screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -57,7 +57,7 @@ const CartScreen = ({ match, location, history }) => {
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>₹{(item.price * 72.91).toFixed(2)}</Col>
+                  <Col md={2}>${item.price.toFixed(2)}</Col>
                   <Col md={2}>
                     <Form.Control
                       as="select"
@@ -98,9 +98,9 @@ const CartScreen = ({ match, location, history }) => {
                 Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                 items
               </h2>
-              ₹
+              $
               {cartItems
-                .reduce((acc, item) => acc + item.qty * item.price * 72.91, 0)
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
                 .toFixed(2)}
             </ListGroup.Item>
             <ListGroup.Item>
