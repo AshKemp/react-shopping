6d969f996f66bca7de292bebe5fbe8c7e099f3c0
Ashwin Km <kmuraliashwin@gmail.com>
2021-03-15 14:08:12 +0530

    Defined the backend endpoint to update order to paid

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require("../controller/orderController.js");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").post(protect, addOrderItems);
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 module.exports = router;
