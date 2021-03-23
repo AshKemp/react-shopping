730cb5ef23a87dc760b6bd724cb948230acf66fa
Ashwin Km <kmuraliashwin@gmail.com>
2021-03-23 22:18:21 +0530

    registered the GET route to fetch users which is protected and only admins can view

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,10 +5,11 @@ const {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } = require("../controller/userController.js");
-const { protect } = require("../middleware/authMiddleware");
+const { protect, admin } = require("../middleware/authMiddleware");
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router
   .route("/profile")
