b6645761955099aa6581d79fca77d4512a860355
Ashwin Km <kmuraliashwin@gmail.com>
2021-03-27 11:17:16 +0530

    dispatched the delete user action on clicking of delete button. added window alert for confirmation

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { listUsers } from "../actions/userActions";
+import { listUsers, deleteUser } from "../actions/userActions";
 
 const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
@@ -16,16 +16,22 @@ const UserListScreen = ({ history }) => {
 
   const { userInfo } = userLogin;
 
+  const userDelete = useSelector((state) => state.userDelete);
+
+  const { success: successDelete } = userDelete;
+
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers());
     } else {
       history.push("/login");
     }
-  }, [dispatch, history]);
+  }, [dispatch, history, successDelete]);
 
   const deleteHandler = (id) => {
-    console.log("delete");
+    if (window.confirm("Are you sure you want to delete ?")) {
+      dispatch(deleteUser(id));
+    }
   };
 
   return (
