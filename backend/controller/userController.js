97140411b5afa242f13e7dc94320251bd804b292
Ashwin Km <kmuraliashwin@gmail.com>
2021-03-27 11:45:19 +0530

    defined 2 controller methods to fetch user by id and update an user by id for admins

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -105,6 +105,37 @@ const deleteUser = asyncHandler(async (req, res) => {
   res.json(users);
 });
 
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+  res.send("Success");
+});
+
 module.exports = {
   authUser,
   registerUser,
@@ -112,4 +143,6 @@ module.exports = {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
