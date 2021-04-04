97b68ce1a7ea31614fc696c66d75b6f13d543e00
Ashwin Km <kmuraliashwin@gmail.com>
2021-04-04 20:57:38 +0530

    Modified the get products controller method to search based on keyword, case insensitive

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -2,7 +2,16 @@ const asyncHandler = require("express-async-handler");
 const Product = require("../model/productModel");
 
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const products = await Product.find({ ...keyword });
   res.json(products);
 });
 
