86f14ab4725d62cc7f1a0285207682ff29ef5155
Ashwin Km <kmuraliashwin@gmail.com>
2021-04-06 15:36:24 +0530

    Registered the top rated product reducer in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ import {
   productCreateReducer,
   productUpdateReducer,
   productReviewCreateReducer,
+  productTopRatedReducer,
 } from "./reducers/productListReducer";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -35,6 +36,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productTopRated: productTopRatedReducer,
   productReviewCreate: productReviewCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
