637ae4b464559537dfa18e56796d10dde157583b
Ashwin Km <kmuraliashwin@gmail.com>
2021-04-06 10:01:09 +0530

    Injected the pagination component in Homescreen, dispatch keyword and pagenumber

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,15 +7,16 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 import Loader from "../components/Loader";
+import Pageinate from "../components/Pageinate";
 import Message from "../components/Message";
 
 const HomeScreen = ({ match }) => {
   // const [products, setProducts] = useState([]);
   const keyword = match.params.keyword;
-
+  const pageNumber = match.params.pageNumber || 1;
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
-  const { loading, products, error } = productList;
+  const { loading, products, error, page, pages } = productList;
   useEffect(() => {
     // const fetchProducts = async () => {
     //   const { data } = await axios.get("/api/products");
@@ -23,8 +24,8 @@ const HomeScreen = ({ match }) => {
     //   setProducts(data);
     // };
     // fetchProducts();
-    dispatch(listProducts(keyword));
-  }, [dispatch, keyword]);
+    dispatch(listProducts(keyword, pageNumber));
+  }, [dispatch, keyword, pageNumber]);
 
   return (
     <>
@@ -34,13 +35,20 @@ const HomeScreen = ({ match }) => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Pageinate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ""}
+          />
+        </>
       )}
     </>
   );
